c5a890c98dd7b0878219233d08c0dabc5a916234
opapy <opapy.worker@gmail.com>
2018-11-06 21:03:01 +0900

    fix super method params

--- django_elastipymemcache/client.py
+++ django_elastipymemcache/client.py
@@ -4,7 +4,7 @@ from pymemcache.client.hash import HashClient
 class Client(HashClient):
     def get_many(self, keys, gets=False, *args, **kwargs):
         # pymemcache's HashClient may returns {'key': False}
-        end = super(Client, self).get_many(keys, gets, args, kwargs)
+        end = super(Client, self).get_many(keys, gets, *args, **kwargs)
 
         return {key: end.get(key) for key in end if end.get(key)}
 
